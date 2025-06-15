be1c33507a13cbcb08d9389bea676206ee5534b1
0tak2 <0tak2.code@gmail.com>
2025-06-16 01:09:05 +0900

    Feat: 백엔드 구현

--- week2/bob/backend/main.py
+++ week2/bob/backend/main.py
@@ -1,15 +1,26 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+
+from nlp import *
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/morphs")
+def extract_morphs(input: Union[str, None] = None):
+    if input is not None:
+        content = get_morphs(input=input)
+        return JSONResponse(content=content, media_type="application/json; charset=utf-8")
+    else:
+        raise HTTPException(status_code=400, detail="No input")
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/tags")
+def extract_morphs(input: Union[str, None] = None):
+    if input is not None:
+        content = get_tags(input=input)
+        return JSONResponse(content=content, media_type="application/json; charset=utf-8")
+    else:
+        raise HTTPException(status_code=400, detail="No input")
