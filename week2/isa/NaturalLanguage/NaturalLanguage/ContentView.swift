2ebcf080097928025f691b67f9e3863fd682bce1
Isa <dn0992@naver.com>
2025-06-16 01:51:38 +0900

    Set: 2주차 프로젝트 세팅

---
+++ week2/isa/NaturalLanguage/NaturalLanguage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NaturalLanguage
+//
+//  Created by 우정아 on 6/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ week2/isa/NaturalLanguage/NaturalLanguage/NaturalLanguageApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaturalLanguageApp.swift
+//  NaturalLanguage
+//
+//  Created by 우정아 on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct NaturalLanguageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
