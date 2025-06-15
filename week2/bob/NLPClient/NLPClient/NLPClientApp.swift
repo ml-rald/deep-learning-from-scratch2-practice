0d01bb4b439eb805a7b32b2cdb4081616bbf0316
0tak2 <0tak2.code@gmail.com>
2025-06-16 01:35:09 +0900

    Feature: 클라이언트

---
+++ week2/bob/NLPClient/NLPClient/NLPClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  NLPClientApp.swift
+//  NLPClient
+//
+//  Created by 임영택 on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct NLPClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ week2/bob/NLPClient/NLPClientTests/NLPClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  NLPClientTests.swift
+//  NLPClientTests
+//
+//  Created by 임영택 on 6/16/25.
+//
+
+import Testing
+@testable import NLPClient
+
+struct NLPClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
